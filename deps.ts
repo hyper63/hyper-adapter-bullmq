761f9b14292b7b59508c4c170ada5d1761bb1e62
Tyler Hall <tyler@hyper.io>
2023-12-03 23:11:00 +0000

    chore: bump deps #2

--- deps.ts
+++ deps.ts
@@ -1,9 +1,10 @@
-export * as R from 'https://cdn.skypack.dev/ramda@0.29.0?dts'
+// @deno-types="npm:@types/ramda@^0.29.9"
+export * as R from 'npm:ramda@0.29.1'
 /**
  * Shim hand-rolled crocks types
  */
 // @deno-types="./crocks.d.ts"
-export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
+export { default as crocks } from 'npm:crocks@0.12.4'
 
 export { hmac } from 'https://deno.land/x/hmac@v2.0.1/mod.ts'
 
@@ -11,7 +12,16 @@ export { type Job, type JobType, Queue, Worker } from 'npm:bullmq@4.12.7'
 export { Cluster, Redis } from 'npm:ioredis@5.3.2'
 export { default as cuid } from 'npm:cuid@3.0.0'
 
-export {
+import {
   HyperErr,
-  isHyperErr,
-} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.1/packages/utils/hyper-err.js'
+  isHyperErr as isHyperErrBase,
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.2/packages/utils/hyper-err.js'
+
+export { HyperErr }
+
+/**
+ * The new ramda types in hyper-utils are overly assuming, so
+ * just wrap the isHyperErr from utils with a more unassuming signature
+ */
+// deno-lint-ignore no-explicit-any
+export const isHyperErr = (v: any) => isHyperErrBase(v)
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,12 +1,12 @@
-export { default as pluginFactory } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.0/packages/core/utils/plugin-schema.ts'
+export { default as pluginFactory } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.2/packages/core/utils/plugin-schema.ts'
 export { cache as cachePort } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-port-cache%40v2.0.0/packages/port-cache/mod.ts'
 
 // std lib deps
-export { deferred } from 'https://deno.land/std@0.204.0/async/deferred.ts'
+export { deferred } from 'https://deno.land/std@0.208.0/async/deferred.ts'
 export {
   assert,
   assertEquals,
   assertObjectMatch,
   assertRejects,
   assertThrows,
-} from 'https://deno.land/std@0.204.0/testing/asserts.ts'
+} from 'https://deno.land/std@0.208.0/assert/mod.ts'
