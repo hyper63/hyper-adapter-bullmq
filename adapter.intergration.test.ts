c81f7c7d33bbfb47fc159afeadcb8c8b54f95631
Tyler Hall <tyler@hyper.io>
2024-01-19 01:11:36 +0000

    test: begin test suite

---
+++ adapter.intergration.test.ts
@@ -0,0 +1,54 @@
+import { suite } from './test/suite.ts'
+
+import bullmq from './mod.ts'
+
+Deno.test({
+  name: 'BullMQ Adapter Test Suite - Integration',
+  fn: async (t) => {
+    const url = Deno.env.get('REDIS_URL')
+    if (!url) {
+      throw new Error('Redis connection string is required at REDIS_URL')
+    }
+
+    const JOB = { type: 'FOOBAR', payload: { id: 1 } }
+
+    /**
+     * Load configuration
+     */
+    const {
+      queueRedisClient,
+      workerRedisClient,
+      createQueue,
+      createWorker,
+      concurrency,
+      failedTtl,
+      keyPrefix,
+    } = await bullmq({ url, options: { keyPrefix: 'integration' } }).load()
+
+    /**
+     * Execute the test suite, injecting our configuration
+     */
+    await suite(t, { target: 'http://localhost:3000/hooks/jobs', job: JOB })({
+      queueRedisClient,
+      workerRedisClient,
+      /**
+       * Mock fetch in the test suite
+       */
+      fetch: undefined,
+      createQueue,
+      createWorker,
+      concurrency,
+      failedTtl,
+      keyPrefix,
+    }, { shouldBaseLine: true })
+  },
+  /**
+   * We ignore sanitization of resources
+   * and ops, when running integration tests,
+   * so connections can be lazily destroyed
+   *
+   * See https://deno.com/manual/basics/testing/sanitizers
+   */
+  sanitizeOps: false,
+  sanitizeResources: false,
+})
---
+++ test/suite.ts
@@ -0,0 +1,136 @@
+// deno-lint-ignore-file no-explicit-any require-await
+import { assert, assertEquals, assertObjectMatch } from '../dev_deps.ts'
+
+import { adapter } from '../adapter.ts'
+import { ImplConfig } from '../types.ts'
+
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
+
+export const suite = (
+  t: Deno.TestContext,
+  { queue = 'jobs', job, target }: { queue?: string; job: Record<string, any>; target: string },
+) =>
+async (
+  config: Optional<ImplConfig, 'fetch'>,
+  options: { shouldBaseLine: boolean } = { shouldBaseLine: false },
+) => {
+  /**
+   * This will allow us to mock fetch, so we can
+   * assert calls to a worker endpoint, without actually sending calls across the network
+   */
+  let innerMockFetch = async (_url: string, _options: any) =>
+    new Response(JSON.stringify({ ok: false, msg: 'Not Implemented ' }), { status: 501 })
+
+  const mockFetch = (url: string, options: any) => innerMockFetch(url, options)
+
+  config.fetch = config.fetch || mockFetch as typeof fetch
+
+  const isMockFetch = config.fetch === mockFetch
+  const a = adapter(config as ImplConfig)
+
+  /**
+   * We may need to baseline our test suite by deleting the queue under
+   * test. This step does just that, idempotently
+   */
+  await t.step('baseline', async () => {
+    if (!options.shouldBaseLine) return assert(true)
+
+    const res = await a.delete(queue).then((res: any) => {
+      /**
+       * Database does not exist, so nothing to baseline
+       */
+      if (res.status === 404) return { ok: true }
+      return res
+    })
+
+    assert((res as any).ok)
+  })
+
+  await t.step('index', async (t) => {
+    await t.step('should return an emtpy array if no queues exist', async () => {
+      const res = await a.index()
+      assertObjectMatch(res as any, { ok: true, queues: [] })
+    })
+
+    await t.step('should return all queues', async () => {
+      await a.create({ name: 'foobar', target })
+      await a.create({ name: 'fizzbuzz', target })
+
+      const res = await a.index()
+      assertObjectMatch(res as any, { ok: true, queues: ['fizzbuzz', 'foobar'] })
+
+      await Promise.all([
+        a.delete('foobar'),
+        a.delete('fizzbuzz'),
+      ])
+    })
+  })
+
+  await t.step('create', async (t) => {
+    await t.step('should create the queue', async () => {
+      const res = await a.create({ name: queue, target })
+      assertObjectMatch(res as any, { ok: true })
+
+      await a.delete(queue)
+    })
+
+    await t.step('should return a HyperErr(409) if the queue already exists', async () => {
+      await a.create({ name: queue, target })
+
+      const res = await a.create({ name: queue, target })
+      assertObjectMatch(res as any, { ok: false, status: 409 })
+
+      await a.delete(queue)
+    })
+  })
+
+  await t.step('delete', async (t) => {
+    await t.step('should delete the queue', async () => {
+      await a.create({ name: queue, target })
+
+      const res = await a.delete(queue)
+      assertObjectMatch(res as any, { ok: true })
+    })
+
+    await t.step('should return a HyperErr(404) if the queue does not exist', async () => {
+      const res = await a.delete(queue)
+      assertObjectMatch(res as any, { ok: false, status: 404 })
+    })
+  })
+
+  await t.step('post', async (t) => {
+    await t.step('should post the job to the worker', async () => {
+      await a.create({ name: queue, target, secret: 'foobar' })
+
+      let worker = Promise.resolve()
+
+      /**
+       * Use mock fetch to assert the data sent to the
+       * worker
+       */
+      if (isMockFetch) {
+        worker = new Promise((resolve) => {
+          innerMockFetch = async (url: string, options: any) => {
+            assertEquals(url, target)
+            assertEquals(options.method, 'POST')
+            assertEquals(JSON.parse(options.body), job)
+            assertEquals(options.headers['Content-Type'], 'application/json')
+            assert(options.headers['X-HYPER-SIGNATURE'])
+
+            resolve(undefined)
+
+            return new Response('OK', { status: 200 })
+          }
+        })
+      }
+
+      const res = await a.post({ name: queue, job })
+
+      assert((res as any).ok)
+      assert((res as any).id)
+      await worker
+
+      await a.delete(queue)
+    })
+  })
+}
